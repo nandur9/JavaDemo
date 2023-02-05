870ac23b07a48e03550f2c05a230e3bf715af993
kumarreddy <Nandur9@gmail.com>
2023-02-05 19:01:15 +0000

    TwoDimArray

---
+++ Git.Hub/TwoDimArray.java
@@ -0,0 +1,26 @@
+
+public class TwoDimArray {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int twoDarr[][];
+		twoDarr =new  int[4][5];
+		int k=0;
+		for (int i = 0; i<4;i++)
+		{
+			for (int j=0;j<5;j++)
+			{
+				twoDarr[i][j]=k;
+				System.out.println("value of twoDarr istwoDarr["+i+"["+j+"]"+twoDarr[i][j]);
+				k ++;
+				
+			}
+			//System.out.println("");
+			
+		}
+		
+
+	}
+
+}
