ef17b86cbff5674d1f4aba172f3c76535e0081e6
kumarreddy <Nandur9@gmail.com>
2023-02-05 19:11:41 +0000

    Registration

---
+++ Git.Hub/Registration.java
@@ -0,0 +1,39 @@
+
+public class Registration {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+				
+				String name = "Narendra reddy";  // Declaration and initialization
+				String Company = "Gsrconstractions";
+				String gmail = "Nandur9@gmail.com";
+				
+				 // Declaration of variable
+				 // initialization of variable
+				
+				System.out.println("Value of variable name is :"+ name);
+				System.out.println("Value of variable Company is :"+ Company);
+				System.out.println("Value of variable email is :"+ gmail);
+				
+				
+				
+				// Value of variable can change during execution of program
+				
+				
+				name = "Kumar reddy";            // Declaration and initialization
+				Company = "Bsr infra india ltd";
+				gmail = "Narendrareddynp9@gmail.com";
+				
+				System.out.println("Value of variable name is :"+ name);
+				System.out.println("Value of variable Company is :"+ Company);
+				System.out.println("Value of variable email is :"+ gmail);
+				
+				
+				
+			}
+
+
+
+	}
+
+
