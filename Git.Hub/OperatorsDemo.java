01cf441000a54a5008fef653e9fa290be74c8d99
kumarreddy <Nandur9@gmail.com>
2023-02-05 19:16:07 +0000

    OperatorsDemo

---
+++ Git.Hub/OperatorsDemo.java
@@ -0,0 +1,29 @@
+
+public class OperatorsDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	
+				//increment_ops();
+				//relational_ops();
+				//logical_ops();
+				
+				System.out.println(12+18);
+				System.out.println(20+"30");
+				System.out.println("40"+30);
+				System.out.println("12"+"18");
+
+			}
+		    public static void contamination()
+			{
+				System.out.println(12+18);
+				System.out.println(20+"30");
+				System.out.println("40"+30);
+				System.out.println("12"+"18");
+			}
+
+
+	}
+
+
