4c4c8bd40b33f3b2902d2ce7008ef3c30d00be7c
kumarreddy <Nandur9@gmail.com>
2023-02-05 19:10:14 +0000

    RegistrationSelf

---
+++ Git.Hub/RegistrationSelf.java
@@ -0,0 +1,31 @@
+
+public class RegistrationSelf {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+				
+				String name = "Narendra kumar reddy";
+				String college = "JNTUK";
+				String emailid = "nandur9@gmail.com";
+				long phone_number = 7829212222L;
+				String Branch ="Civil engg";
+				String Rollnumber = "10X25A0101";
+				Short year_of_passout = 2013;
+				
+				
+				 System.out.println("name of student :"+name);
+				 System.out.println("college :"+college);
+				 System.out.println("emailid :"+emailid);
+				 System.out.println("phone_number :"+phone_number);
+				 System.out.println("Branch :"+Branch);
+				 System.out.println("Rollnumber :"+Rollnumber);
+				 System.out.println("year_of_passout :"+year_of_passout);
+				
+
+			}
+
+		}
+
+
+	
