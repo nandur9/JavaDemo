cd540bbe35ef4e1d1ff8847578c060939275ee4d
kumarreddy <Nandur9@gmail.com>
2023-02-05 19:26:28 +0000

    HelloNarendra

---
+++ Git.Hub/HelloNarendra.java
@@ -0,0 +1,25 @@
+
+public class HelloNarendra {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	             // method
+				 // Statements
+				
+				System.out.println("This is my first java program");
+				
+				
+				int data =1001;
+				 
+				//<data type> variable_name = value
+				
+				System.out.println(data);		
+
+			}
+
+		}
+
+
+	
+
+
