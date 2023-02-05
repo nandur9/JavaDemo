728d0e39e5b5d408e3fe57b8e3718491065d9c6f
kumarreddy <Nandur9@gmail.com>
2023-02-05 19:30:58 +0000

    ForLoopDemo

---
+++ Git.Hub/ForLoopDemo.java
@@ -0,0 +1,73 @@
+
+public class ForLoopDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*
+		 int num1;
+		
+		for(num1=10; num1>1; num1--)
+		{
+			System.out.println("Welcome to java programming:"+num1);
+			
+		}
+		    System.out.println("statement out side for loop");
+		  
+		   
+		
+		
+		int num;
+		boolean isprime;
+		num =7;
+		if (num <2)
+			isprime =false;
+		else
+			isprime = true ;
+		for (int i =2; i<=num / i;i++)
+		{
+			if ((num%i==0))
+			{
+				isprime = false;
+				break;
+				
+			}
+		}
+		if (isprime)
+		{
+			System.out.println("Number is prime number");
+		}
+		else
+			System.out.println("Number is not prime number");
+		
+				
+				int a,b;
+				b=5;
+				for (a=1;a<b;a++)
+				{
+					System.out.println("value of a :"+a);
+					System.out.println("value of a :"+b);
+					//b --;
+					if (a ==2);}*/
+		int a,b ;
+		for (a=1,b =6; a<b;a++,b--)
+		{
+			if (a ==1)
+			{
+				//continue
+		
+				
+			}
+			System.out.println("value of a:"+a);
+			System.out.println("value of b:"+b);
+			}
+	 
+		
+		
+				
+   }
+}
+		    
+		    
+		    
+	
+
