f948503398a8dfb08b15bec7553618647958f4ed
kumarreddy <Nandur9@gmail.com>
2023-02-05 19:19:33 +0000

    hi am nanu

---
+++ Git.Hub/nandu.java
@@ -0,0 +1,35 @@
+
+public class nandu {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//public static void relational_ops()
+	    {
+	          int a =100;
+	          int b =150;
+	          // < > <= >=
+	          System.out.println(a<b);
+	          System.out.println(a>b);
+	          System.out.println(a<=b);
+	          System.out.println(a>=b);
+	     
+	  
+		//public static void logical_ops()
+	    {
+	    	// && ||
+	    	
+	    	System.out.println(true && true);
+	    	System.out.println(true && false);
+	    	System.out.println(true);
+	    	System.out.println(true);
+	    	System.out.println(false || false);
+	    	
+	    }
+	    
+	    }
+	}}
+
+
+
+
