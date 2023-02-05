c4f72c43f91475a8b92388297a0f7317bc4d6918
kumarreddy <Nandur9@gmail.com>
2023-02-05 18:28:26 +0000

    WhileDemo0

---
+++ Dec2022/WhileDemo0.java
@@ -0,0 +1,20 @@
+
+public class WhileDemo0 {
+	
+	public static void main(String[] args) {
+			// TODO Auto-generated method stub
+			
+		      int num =10;
+			  while (num>0)  // eval condition)==true 
+			{
+				      // if condition is true, statement inside while loop is executed
+				System.out.println("this is first line Java program :"+ num);
+				num--;
+			}
+	            // if condition is false, then next line immediately following
+			    // loop will be executed
+		}
+	}
+
+
+
