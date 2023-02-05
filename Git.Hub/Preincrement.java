fcee202ce0f5bf8a5260045861541d83f4a3c4af
kumarreddy <Nandur9@gmail.com>
2023-02-05 19:13:12 +0000

    Preincrement

---
+++ Git.Hub/Preincrement.java
@@ -0,0 +1,17 @@
+
+public class Preincrement {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+				int a =555;
+				System.out.println(++a);
+
+			}
+
+
+
+	}
+
+
