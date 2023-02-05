13e6d71dc1971a037856bdb3d19a636ac7fd3523
kumarreddy <Nandur9@gmail.com>
2023-02-05 19:28:18 +0000

    GetVarible

---
+++ Git.Hub/GetVarible.java
@@ -0,0 +1,24 @@
+
+public class GetVarible {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		// Declare a variable
+				
+				int a;
+				
+				//intialigation of a veritable
+				
+				a = 18888;
+				System.out.println(a);
+				
+
+			}
+
+		}
+
+
+	
+
+
