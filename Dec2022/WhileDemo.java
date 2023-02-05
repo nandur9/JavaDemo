77e8eb5e3e9f7b02d9a795c86a81b2cc05d1ce3e
kumarreddy <Nandur9@gmail.com>
2023-02-05 18:31:30 +0000

    WhileDemo

---
+++ Dec2022/WhileDemo.java
@@ -0,0 +1,22 @@
+
+public class WhileDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int num =8;
+		while (num>0)
+		{
+			System.out.println("welcome to java programing :"+ num);
+			num--;
+		}
+		System.out.println("First statement out side while loop");
+		System.out.println("I love u java programing language");
+
+	          int i =10;
+	          int j =20;
+	    
+	          while (++i < --j);
+	            System.out.println("Mid point is "+i);
+
+}}
