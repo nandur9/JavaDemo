1d82351177726c2e762a34420dbcaeb61bcada5a
kumarreddy <Nandur9@gmail.com>
2023-02-05 18:51:28 +0000

    ArraysDemo

---
+++ Git.Hub/ArraysDemo.java
@@ -0,0 +1,30 @@
+
+public class ArraysDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		int month_days[];
+		month_days = new int [5];
+		month_days [1] =10; 
+		month_days [4] =20;
+		   //type name_arr[]
+		   //name_arr = new type[size]
+		//month_day = new int[j];
+		System.out.println(month_days[0]);
+		System.out.println(month_days[1]);
+		System.out.println(month_days[2]);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
