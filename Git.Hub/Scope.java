2d0a6ae0ef0b45fee185d25e19b52c610db74a77
kumarreddy <Nandur9@gmail.com>
2023-02-05 19:08:26 +0000

    Scope

---
+++ Git.Hub/Scope.java
@@ -0,0 +1,23 @@
+
+public class Scope {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int x ;
+		x = 100;
+		if (x == 100)
+		{
+			 int y =200; 
+			 System.out.println("value of y:"+y);
+		}
+		     //y =30;
+		     System.out.println("value ofxy:"+x);
+		     
+		
+		
+		}
+
+	}
+
+
