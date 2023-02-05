ee0a3960b1b7c96b3e059cc3aca2dddae65bf199
kumarreddy <Nandur9@gmail.com>
2023-02-05 18:58:28 +0000

    VariablesDemo

---
+++ Git.Hub/VariablesDemo.java
@@ -0,0 +1,47 @@
+
+public class VariablesDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		// Declare a variable and initialization
+				
+				int a = 90;
+				
+				System.out.println(a);
+				
+				int b = 229;
+				 System.out.println(b);
+				 
+			    float c = 34.4f;
+				 System.out.println(c);
+				 
+				char ch = 'N';
+				 System.out.println(ch);
+				
+				boolean result = true;
+				System.out.println(result);
+				
+				boolean result1 = false;
+				System.out.println(result1);
+				
+				// Reinitialize to other values
+				
+				  System.out.println("---reasign new values to variable of 4 different data types");
+				
+				a = 2023;
+				System.out.println(a);
+				
+				c = 99.99f;
+				System.out.println(c);
+				
+				ch =  'M';
+				System.out.println(ch);
+				
+				result = false;
+				System.out.println(result);
+
+
+	}}
+
+
