5dc634ba0e1093a5cc75f5fc3035e15f235e953f
kumarreddy <Nandur9@gmail.com>
2023-02-05 19:21:32 +0000

    Increment

---
+++ Git.Hub/Increment.java
@@ -0,0 +1,23 @@
+
+public class Increment {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	
+		
+		      int a =555;
+		        System.out.println(++a);
+		
+		      int b = 88;
+		
+		        System.out.println(b++);
+		
+		      int c = b;
+		
+		        System.out.println(c);
+		
+
+
+	}
+
+}
