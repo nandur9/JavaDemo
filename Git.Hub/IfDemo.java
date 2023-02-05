4bd71a90c28ecace2644aa5834929a6ef2edc864
kumarreddy <Nandur9@gmail.com>
2023-02-05 19:24:53 +0000

    If Demo

---
+++ Git.Hub/IfDemo.java
@@ -0,0 +1,23 @@
+
+public class IfDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	            int age = 37;
+				 if (age> 35)
+				 {
+					
+					System.out.println("old");
+				 }
+				 else
+				 {
+					 System.out.println("young"); 
+				 }
+
+			}
+
+
+}
+
+
