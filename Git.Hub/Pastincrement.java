614b1d5b54d900da3d1b004c9a34131393361fcd
kumarreddy <Nandur9@gmail.com>
2023-02-05 19:14:51 +0000

    Pastincrement

---
+++ Git.Hub/Pastincrement.java
@@ -0,0 +1,22 @@
+
+public class Pastincrement {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+				int b = 88;
+				
+				System.out.println(b++);
+				
+				int c = b;
+				
+				System.out.println(c);
+
+			}
+
+		}
+
+
+	
+
+
