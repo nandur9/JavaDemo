d04ff53b3dc22fbbc6715dee212f279076a82f57
kumarreddy <Nandur9@gmail.com>
2023-02-06 01:12:15 +0000

    Dowhile

---
+++ Git.Hub/DynamicIntilization.java
@@ -0,0 +1,13 @@
+
+public class DynamicIntilization {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		double a = 50000, b = 90000;
+		double c = Math.sqrt(a*a + b*b);
+		System.out.println("value of c is :"+c);
+
+	}
+
+}
