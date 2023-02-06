dbd189281770a6997985a2862c2537fb1e48ccd8
kumarreddy <Nandur9@gmail.com>
2023-02-06 00:58:45 +0000

    Box1

---
+++ Git.Hub/Box1.java
@@ -0,0 +1,66 @@
+
+public class Box1 {
+	   
+	
+	
+		double width; 
+		double height;  
+		double depth;
+		  void setDimension(double w,double h,double d) {
+		
+			  double volume ;
+		  
+			  width = w;
+			  height = h;
+			  depth = d;
+		 
+		 
+		  
+			 System.out.print("volume is:");
+			 System.out.println(width * height * depth);
+		 
+         // return width * height * depth ;
+		
+		  }
+		
+			
+}
+
+
+		 
+		
+		/*Box1(double w,double h ,double d)
+		{
+			System.out.println("Constructing a box");
+			width=w;
+			height=h;
+			depth=d;
+		}
+		
+		double  volume()
+		{
+					
+		 return width * height * depth;
+			
+			
+		}
+
+	}
+
+	class <<name of class >>
+	{
+		type instance_variabe1;
+		type instance_variabe2;
+		
+		......
+		type instance_variableN
+		
+		type method1()
+		type method2()
+		
+		  instance variables + methods = members of the class 
+	}
+
+	*/
+
+
