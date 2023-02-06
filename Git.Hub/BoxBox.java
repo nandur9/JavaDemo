4fdacd25aec7553efd271f19d477acac8151f70f
kumarreddy <Nandur9@gmail.com>
2023-02-06 14:37:27 +0000

    constructing

---
+++ Git.Hub/BoxBox.java
@@ -0,0 +1,22 @@
+
+class BoxBox {
+	
+    public static void main (String[] args) {
+    	
+    	double volume;
+    	
+    	Box2 b1 = new Box2();
+    	
+            volume = b1.volume()
+            		;
+    	    System.out.println("volume is"+ volume);
+    	    
+    	    Box2 b2 = new Box2();
+    	
+    	    volume= b2.volume();
+    	    
+    	    System.out.println("volume is"+ volume);
+    }
+}
+
+	
