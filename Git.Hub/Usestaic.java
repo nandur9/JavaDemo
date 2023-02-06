81f3977b1a495b89041934d0b677f246f5bcfaa8
kumarreddy <Nandur9@gmail.com>
2023-02-06 17:07:58 +0000

    Usestaic

---
+++ Git.Hub/Usestaic.java
@@ -0,0 +1,23 @@
+
+public class Usestaic  { 
+	
+	    static int a =1;
+	    static int b;
+	    
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		method01(5);
+
+	}
+	    static void method01 (int x)
+	{
+		System.out.println("volume of x "+ x);
+		System.out.println("volume of q "+ a);
+		System.out.println("volume of b"+ b);
+	}
+	    static {
+	    	b = a *2;}
+	    }
+
+
