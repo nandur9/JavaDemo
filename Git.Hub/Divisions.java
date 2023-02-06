ecfcfa537a87c84bf6234e92b2a30e5e459897f6
kumarreddy <Nandur9@gmail.com>
2023-02-06 01:07:25 +0000

    Divisions

---
+++ Git.Hub/Divisions.java
@@ -0,0 +1,22 @@
+
+public class Divisions {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+				int marks = 35;
+				if (marks > 55)
+					 
+					System.out.print("First Division");
+				
+				   else if (marks >= 45  &&  marks <= 55)
+		             System.out.print("Second Division");
+		           else
+		        	   System.out.print("Third Division");
+		        	   
+		        	   
+				
+			}
+
+		}
