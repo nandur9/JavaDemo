2cfa4ff82d04dcb64e3156ac1f8a67c6c2ca43df
kumarreddy <Nandur9@gmail.com>
2023-02-08 02:36:13 +0000

    AccessControlTest

---
+++ Git.Hub/AccessControlTest.java
@@ -0,0 +1,17 @@
+
+public class AccessControlTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+         AccessControlDemo obj1= new AccessControlDemo();
+		
+		obj1.a =10;
+		obj1.b =20;
+		obj1.setC (30);
+		    System.out.println("volume of a:"+obj1.a);
+		    System.out.println("volume of b:"+obj1.b);
+		    System.out.println("volume of c:"+obj1.getC);
+
+	}
+
+}
